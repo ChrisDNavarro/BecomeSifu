94e4af192ef800fb69020fa5c94aa05b553ad527
Chris Navarro <72816161+cn250039@users.noreply.github.com>
2022-06-07 13:28:33 -0500

    closer still

---
+++ Controls/BoostsController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BecomeSifu.Controls
+{
+    class BoostsController
+    {
+        public int boost { get; set; } = 1;
+
+
+    }
+}
